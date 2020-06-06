4402146d1db87c3e167e8c9629dea47699ae090b
Aditya Saraf <sarafa@cs.uw.edu>
2020-06-06 12:30:28 -0700

    Added some batching for the LSTM and BaumWelch to learn hmm parameters

--- lstm_attack.py
+++ lstm_attack.py
@@ -2,124 +2,130 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 import hmms
 import inference_attack
+import BaumWelch
 import sys
-
-torch.manual_seed(1)
 
 class BDPLSTM(nn.Module):
 
-    def __init__(self, input_dim = 1, hidden_dim = 10, output_dim = 1):
+    def __init__(self, hidden_dim = 4, output_dim = 1, embedding_dim = 4):
         super(BDPLSTM, self).__init__()
 
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.embedding = nn.Embedding(2, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)#, bidirectional=True)
         self.linear = nn.Linear(hidden_dim, output_dim)
-        self.sigmoid = nn.Sigmoid()
+        self.output = nn.Sigmoid()
 
 
     # sanitized_db.shape = [batchsize, seq_len, input_dim]
     def forward(self, sanitized_db):
-        lstm_out, _ = self.lstm(sanitized_db.view(1, sanitized_db.size()[0], 1))
+        x = self.embedding(sanitized_db)
+        x, _ = self.lstm(x)
         # [batchsize, seq_len, hidden_dim]
-        lstm_out = lstm_out[:,-1,:] # just keep last lstm output
-        linear_out = self.linear(lstm_out)
-        prob = self.sigmoid(linear_out)
-        return prob.reshape(1)
+        x = x[:,-1,:] # just keep last lstm output
+        x = self.linear(x)
+        prob = self.output(x)
+        return prob
 
     # probs, targets shape: [batch_size, 1]
     def loss(self, probs, targets):
-        return nn.functional.binary_cross_entropy(probs, targets)
+        return nn.functional.binary_cross_entropy(probs, targets.float())
 
+    # probs, targets shape: [batch_size, 1]
     def accuracy(self, probs, targets):
-        """
-        Computes the accuracy, i.e number of correct predictions / N.
-        :param probs: Raw predictions from the model of shape (N, 1)
-        :param targets: True labels of shape (N, 1)
-        :return: Accuracy as a scalar tensor.
-        """
         correct = (probs.round() == targets).sum()
         return correct.float()/probs.size(0)    
 
 
 # training_data = [(N, 4), (N, 1)]
-def LSTMAttacker(training_data, num_epochs=20):
+def LSTMAttacker(trainloader, num_epochs=25):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = BDPLSTM()
-    optimizer = optim.Adam(model.parameters(), lr=7e-4)
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=5e-4)
 
-    # last_loss = 1
-    # unimproved_iters = 0
-    # unimproved_tolerance = 10
     for epoch in range(num_epochs):
-        # print(f"Epoch: {epoch}")
         train_correct = 0.
         train_seqs = 0.
         train_loss = 0.0
-        for x, y in training_data:
+        for data in trainloader:
+            x, y = data[0].to(device), data[1].to(device)
             model.zero_grad()
             out = model(x)
             loss = model.loss(out, y) 
             loss.backward()
             optimizer.step()
-            correct = model.accuracy(out, y).item()
+            correct = model.accuracy(out, y).item() * len(out)
             train_loss += loss.item()
             train_correct += correct
-            train_seqs += 1 #len(x)
+            train_seqs += len(x)
 
-        print(f"Loss after epoch {epoch} : {train_loss / train_seqs}")
+        print(f"Loss after epoch {epoch} : {train_loss / len(trainloader)}")
         print(f"Accuracy after epoch {epoch} : {train_correct / train_seqs}")
-        # if (loss.item() < last_loss):
-        #     unimproved_iters = 0
-        # if (loss.item() > last_loss):
-        #     unimproved_iters += 1
-        # if (unimproved_iters > unimproved_tolerance):
-        #     break
-        # if (loss.item() <= 0.001):
-        #     break
 
     return train_correct / train_seqs
-    # cumulative_loss = 0
-    # with torch.no_grad():
-    #     for x, y in list(training_data):
-    #         print(model(x).float().round())
-    #         cumulative_loss += loss_function(model(x).float().round(), y.repeat(x.size()[0], 1).reshape(x.size()[0], -1))
-
-    # return 1.-((cumulative_loss/len(training_data)).item())
 
 num_hidden_states = 1
 num_observations = 1
-seq_len = 5000
+seq_len = 10000
 
 if __name__ == "__main__":
     eps = 0.5
-    theta = 0.2
+    theta = 0.1
     print('eps:', eps, file=sys.stderr)
     dp_noise = inference_attack.diff_privacy_noise(eps)
     print('noise:', dp_noise, file=sys.stderr)
     print('Probability of not flipping state (lower bound)', 1-dp_noise, file=sys.stderr)
     print(f'Upper bound: {1-theta}')
 
-    model = inference_attack.symmetric_hmm(theta, dp_noise)
-    latents, _ = model.generate_data((num_hidden_states, seq_len))
-    size = 4
+    hmm = inference_attack.symmetric_hmm(theta, dp_noise)
+    latents, _ = hmm.generate_data((num_hidden_states, seq_len))
+    size = 20
 
-    training_data_inputs = torch.zeros((num_hidden_states * (seq_len-(size)), size), dtype=torch.float)
-    training_data_outputs = torch.zeros((num_hidden_states * (seq_len-(size)), 1),  dtype=torch.float)
+    training_data_inputs = torch.zeros((num_hidden_states * (seq_len-(size)), size), dtype=torch.long)
+    training_data_outputs = torch.zeros((num_hidden_states * (seq_len-(size)), 1),  dtype=torch.long)
     # collecting data
     for idx, latent in enumerate(latents):
-        sanitized = inference_attack.emissions(latent, model.b)
-        transition_counts = {(True, True): 0, (True, False): 0, (False, True): 0, (False, False): 0}
-        for i in range(len(latent) - 1):
-            from_state = latent[i]
-            to_state = latent[i+1]
-            transition_counts[(from_state, to_state)] += 1
+        sanitized = inference_attack.emissions(latent, hmm.b)
+        
+        # FOR TESTING PURPOSES ONLY
+        # np.save("latent", latent)
+        # np.save("sanitized", sanitized)
+        # latent = np.load("latent.npy")
+        # sanitized = np.load("sanitized.npy")
+        torch.manual_seed(0)
+        np.random.seed(0)
+        
+        _, predictions = hmm.viterbi(sanitized)
+        count = np.sum(predictions == latent)
+        print(f'Viterbi Accuracy (knows parameters): {count/seq_len}')
+        
+        # More accurate, but underflows (should just implement separate algo which maximizes only the transition probabilities)
 
-        q = transition_counts[(False, True)]/(transition_counts[(False, True)] + transition_counts[(False, False)])
-        r = transition_counts[(True, False)]/(transition_counts[(True, False)] + transition_counts[(True, True)])
-        print(f"q: {q}, r: {r}")
-        for inner_idx in range(seq_len-size):
+        rho = dp_noise
+        theta_hats = []
+        for i in range(seq_len//750):
+            a = np.ones((2, 2))
+            a = a / np.sum(a, axis=1)
+            emissions = np.array([[1-rho,rho],[rho,1-rho]])
+            pi = np.array((0.5, 0.5))
+            a, b = BaumWelch.baum_welch(sanitized[i*750:(i+1)*750], a, emissions, pi, n_iter=100)
+            print(f'a:{a} \nb:{b}')
+            theta_hats.append((a[0, 1] + a[1,0])/2)
+        theta_hat = np.average(theta_hats)
+        p_hat = np.array([[1-theta_hat, theta_hat], [theta_hat, 1-theta_hat]])
+        print(f'P_hat = {p_hat}')
+
+        custom_model: hmms.DtHMM = hmms.DtHMM(p_hat, emissions, pi)
+        _, predictions = custom_model.viterbi(sanitized)
+        count = np.sum(predictions == latent)
+        print(f'Viterbi Accuracy (learns parameters with custom BW): {count/seq_len}')
+
+        for inner_idx in range(seq_len-size-2):
             training_data_inputs[idx*(seq_len-(size)) + inner_idx] = torch.tensor(sanitized[inner_idx:inner_idx+size])
-            training_data_outputs[idx*(seq_len-(size)) + inner_idx] = torch.tensor(latent[inner_idx+size-1])
+            training_data_outputs[idx*(seq_len-(size)) + inner_idx] = torch.tensor(latent[inner_idx+size//2])
         
         # observations = []
         # for i in range(num_observations):
@@ -128,8 +134,7 @@ if __name__ == "__main__":
     
 
     training_data = list(zip(training_data_inputs, training_data_outputs))
-    # trainloader = torch.utils.data.DataLoader(training_data, batch_size=b,
-    #                                     shuffle=True, num_workers=2)
-    lstm_accuracy = LSTMAttacker(training_data)
-     
+    trainloader = torch.utils.data.DataLoader(training_data, batch_size=16,
+                                        shuffle=True, num_workers=4)
+    lstm_accuracy = LSTMAttacker(trainloader)
     print(f"LSTM accuracy: {lstm_accuracy}")
